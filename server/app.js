d61dbec7e9c7c0d524f88821c766bdc553fc9ae7
simonteozw <simonteozw@gmail.com>
2022-01-05 00:43:39 +0800

    Set up stream function

--- server/app.js
+++ server/app.js
@@ -18,3 +18,26 @@ const connection = shareDBServer.connect();
  * 'firstDocument' is the id of the document
  */
 const doc = connection.get('documents', 'firstDocument');
+
+doc.fetch(function (err) {
+    if (err) throw err;
+    if (doc.type === null) {
+        /**
+         * If there is no document with id "firstDocument" in memory
+         * we are creating it and then starting up our ws server
+         */
+        doc.create([{ insert: "Hello world!" }], "rich-text", setupStream);
+        return;
+    }
+});
+
+function setupStream() {
+    const wss = new WebSocket.Server({ port: 8080 });
+
+    wss.on("connection", function connection(ws) {
+        // For transport we are using a ws JSON stream for communication
+        // that can read and write js objects.
+        const jsonStream = new WebSocketJSONStream(ws);
+        shareDBServer.listen(jsonStream);
+    });
+}
