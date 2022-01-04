07ff74a705b051adf7a4c72a00d20ff17f759056
simonteozw <simonteozw@gmail.com>
2022-01-04 16:52:48 +0800

    Set up document

--- server/app.js
+++ server/app.js
@@ -2,3 +2,19 @@ const WebSocket = require('ws');
 const WebSocketJSONStream = require('@teamwork/websocket-json-stream');
 const ShareDB = require('sharedb');
 
+/**
+ * By Default Sharedb uses JSON0 OT type.
+ * To Make it compatible with our quill editor.
+ * We are using this npm package called rich-text
+ * which is based on quill delta
+ */
+ShareDB.types.register(require('rich-text').type);
+
+const shareDBServer = new ShareDB();
+const connection = shareDBServer.connect();
+
+/**
+ * 'documents' is collection name(table name in sql terms)
+ * 'firstDocument' is the id of the document
+ */
+const doc = connection.get('documents', 'firstDocument');
