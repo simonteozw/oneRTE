09623ada974d7ea0f9d581ea807c29395a00fc52
simonteozw <simonteozw@gmail.com>
2022-01-05 11:15:15 +0800

    Add useEffect function

--- client/src/App.js
+++ client/src/App.js
@@ -15,3 +15,47 @@ const connection = new Sharedb.Connection(socket);
 // Query for our document
 const doc = connection.get('documents', 'firstDocument');
 
+function App() {
+  useEffect(() => {
+    doc.subscribe(function (err) {
+      if (err) throw err;
+
+      const toolbarOptions = ["bold", "italic", "underline", "strike", "align"];
+      const options = {
+        theme: "bubble",
+        modules: {
+          toilbar: toolbarOptions,
+        }
+      };
+      let quill = new Quill("#editor", options);
+
+      /**
+       * On Initialising if data is present in server
+       * Updaing its content to editor
+       */
+      quill.setContents(doc.data);
+
+      /**
+       * On Text change publishing to our server
+       * so that it can be broadcasted to all other clients
+       */
+      quill.on("text-change", function(delta, onDelta, source) {
+        if (source !== "user") return;
+        doc.submitOp(delta, { source: quill });
+      });
+
+      /** Listen to changes in the document
+       * that is coming from our server
+       */
+      doc.on("op", function (op, source) {
+        if (source === quill) return;
+        quill.updateContents(op);
+      });
+    });
+    return () => {
+      connection.close();
+    }
+  }, []);
+}
+
+export default App;
