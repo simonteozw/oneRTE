16d5efa2447e07dadc7093014e4d294719f4897e
simonteozw <simonteozw@gmail.com>
2022-01-05 09:45:40 +0800

    Set up connection in client

--- client/src/App.js
+++ client/src/App.js
@@ -2,4 +2,12 @@ import React, { useEffect } from 'react';
 import Quill from 'quill';
 import 'quill/dist/quill.bubble.css';
 import Sharedb from 'sharedb/lib/client';
-import richText from 'rich-text'
+import richText from 'rich-text';
+
+// Registering the rich text type to make sharedb work
+// with our quill editor
+Sharedb.types.register(richText.type);
+
+// Connecting to socket server
+const socket = new WebSocket('ws://127.0.0.1:8080');
+const connection = new Sharedb.Connection(socket);
